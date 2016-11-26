ed52d4311f02c6e7168f5e4efeefff5ceeb2da0f
stikompocir <siepetier1991@gmail.com>
2016-11-25 16:41:15 -0800

    test api

---
+++ app/src/main/java/com/stikompocir/maman/loginandroidtestapi/AppConfig.java
@@ -0,0 +1,11 @@
+package com.stikompocir.maman.loginandroidtestapi;
+
+/**
+ * Created by Maman on 11/22/2016.
+ */
+public class AppConfig {
+    public static String URL_LOGIN = "http://localhost/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://localhost/android_login_api/register.php";
+}
